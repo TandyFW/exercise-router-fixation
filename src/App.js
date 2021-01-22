8257eac6a0ea27fc719d89ee69917013ef2a0cf3
TandyFW <tandyfw@gmail.com>
2021-01-22 07:24:46 -0300

    Questão 4 e 5 concluidas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/Home" component={ Home }></Route>
-        <Link to="./Home">Home</Link>
-        <Link to="./About">About</Link>
-        <Link to="./Users">Users</Link>
+        <Route exact path="/" component={ Home }></Route>
+        <Route path="/About" component={ About }></Route>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Users">Users</Link>
       </BrowserRouter>
     );
   }
