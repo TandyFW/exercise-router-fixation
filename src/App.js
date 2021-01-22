f64c645ec257a8b59b205fd2758536f7df53c5ec
TandyFW <tandyfw@gmail.com>
2021-01-22 07:26:03 -0300

    Questão 6 concluida

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Users from './Users';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Home }></Route>
-        <Route path="/About" component={ About }></Route>
+        <Route exact path="/About" component={ About }></Route>
+        <Route exact path="/Users" component={ Users }></Route>
         <Link to="/">Home</Link>
         <Link to="/About">About</Link>
         <Link to="/Users">Users</Link>
